e960e9d11def5763e43842088ab773b8f3e9d86f
David Kariukí <davidkariuki@users.noreply.github.com>
2020-12-28 21:30:56 +0000

    Add sort/limit/skip test

--- tests/findUser.test.ts
+++ tests/findUser.test.ts
@@ -3,14 +3,23 @@ import setupDB from "./setup-tests"
 
 setupDB("findUsers")
 
-describe("Reading User out of the database", () => {
-  let joe: UserDocument
+describe("Reading users out of the database", () => {
+  let joe: UserDocument,
+    alex: UserDocument,
+    maria: UserDocument,
+    zac: UserDocument
 
-  beforeEach(async () => {
-    joe = await User.create({ name: "Joe" })
+  beforeEach(async (done) => {
+    joe = new User({ name: "Joe" })
+    alex = new User({ name: "Alex" })
+    maria = new User({ name: "Maria" })
+    zac = new User({ name: "Zac" })
+
+    await Promise.all([joe.save(), alex.save(), maria.save(), zac.save()])
+    done()
   })
 
-  it("finds all User named Joe", async (done) => {
+  it("finds all users named Joe", async (done) => {
     const dbUser = await User.find({ name: "Joe" })
 
     expect(dbUser[0].id).toEqual(joe.id)
@@ -21,6 +30,15 @@ describe("Reading User out of the database", () => {
     const foundUser = await User.findOne({ _id: joe._id })
 
     expect(foundUser?.name).toEqual(joe.name)
+    done()
+  })
+
+  it("can skip and limit the result set", async (done) => {
+    const users = await User.find({}).sort("name").skip(1).limit(2)
+
+    expect(users.length).toEqual(2)
+    expect(users[0].name).toEqual(joe.name)
+    expect(users[1].name).toEqual(maria.name)
     done()
   })
 })
